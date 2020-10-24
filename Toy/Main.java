4ab41ca36f896d936a237be03d748aefb92b32dd
epittab <ep7m@me.com>
2020-10-23 23:56:16 -0400

    Set up first constructor

---
+++ Toy/Main.java
@@ -0,0 +1,10 @@
+package com.OOP.Toy;
+
+public class Main {
+
+    public static void main(String[] args){
+        Toy powerRanger = new Toy("Green", "Hasbro", 25);
+        System.out.println("The toy has " + powerRanger.getParts() + " parts.");
+    }
+    
+}
--- Toy/Toy.java
+++ Toy/Toy.java
@@ -1,7 +1,20 @@
 package com.OOP.Toy;
 
 public class Toy {
-    public static void main(String[] args){
+    
+    private String model;
+    private String brand;
+    private int numOfParts;
+
+    public Toy (String model, String brand, int numOfParts){
         // code here
+        this.model = model;
+        this.brand = brand;
+        this.numOfParts = numOfParts;
+    }
+    
+
+    public int getParts(){
+        return this.numOfParts;
     }
 }
